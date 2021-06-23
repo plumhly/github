350cc3ef44c99161cccbd5a1935da1ad4172d7fe
plum <>
2021-06-23 23:03:00 +0800

    feat: 登录弹框

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,8 +9,23 @@
 		</view>
 		
 		<view class="bottom_container">
-			<button type="default">使用GitHub登录</button>
+			<button type="default" @click="login">使用GitHub登录</button>
 		</view>
+		<uni-popup ref="popup" type="bottom">
+			<view class="popup">
+				<view class="popup_content">
+					<view class="popup_head">
+						<view>仓库访问权限选项</view>
+						<view>隐私声明：PPHub不会从您的GitHub账号收集任何信息，我甚至连服务器都没有，请您放心使用。</view>
+					</view>
+					<button type="default" @click="onlyAllowPublicRepository">仅公开库</button>
+					<button type="default" @click="allowAll">私有库 + 公开库</button>
+				</view>
+				<view class="popup_cance">
+					<button type="default" @click="dismiss">取消</button>
+				</view>
+			</view>
+		</uni-popup>
 	</view>
 </template>
 
@@ -25,7 +40,21 @@
 
 		},
 		methods: {
-
+			login() {
+				this.$refs.popup.open();
+			},
+			
+			dismiss() {
+				this.$refs.popup.close();
+			},
+			
+			onlyAllowPublicRepository() {
+				
+			},
+			
+			allowAll() {
+				
+			},
 		}
 	}
 </script>
@@ -46,7 +75,7 @@
 			text {
 				float: right;
 				font-size: 24rpx;
-				color: #027cff;
+				color: $uni-text-color;
 				margin: 40rpx 24rpx 0 0;
 			}
 		}
@@ -72,6 +101,47 @@
 			}
 		}
 		
+		.popup {
+			background-color: transparent;
+			padding: 0 16rpx 16rpx;
+			.popup_content {
+				background-color: white;
+				border-radius: 23rpx;
+				overflow: hidden;
+				color: $uni-text-color-grey;
+				text-align: center;
+				font-size: 27rpx;
+				.popup_head {
+					padding: 30rpx 38rpx 48rpx;
+					view:first-child {
+						font-weight: bold;
+						margin-bottom: 17rpx;
+					}
+				}
+								
+				button {
+					border-top: 1px solid $uni-border-color;
+					border-radius: 0;
+					color: $uni-text-color;
+				}
+				
+				button:after { border: none }
+			}
+			
+			.popup_cance {
+				margin-top: 16rpx;
+				font-size: 38rpx;
+				font-weight: bold;
+				
+				button {
+					height: 110rpx;
+					line-height: 110rpx;
+					text-align: center;
+					color: $uni-text-color;
+					border-radius: 23rpx;
+				}
+			}
+		}
 	}
 	
 </style>
